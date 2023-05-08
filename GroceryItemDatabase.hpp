bb621a263bc94a8fd5fb4bde5b23af731e9a839f
KentvTran <111903963+KentvTran@users.noreply.github.com>
2023-05-07 17:00:56 -0700

    Add files via upload

---
+++ GroceryItemDatabase.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+///////////////////////// TO-DO (1) //////////////////////////////
+  /// Include necessary header files
+  /// Hint:  Include what you use, use what you include
+  ///
+  /// Do not put anything else in this section, i.e. comments, classes, functions, etc.  Only #include directives
+//#include "GroceryItem.hpp"
+#include <string>
+#include<cstddef>
+/////////////////////// END-TO-DO (1) ////////////////////////////
+
+
+// Singleton Design Pattern
+class GroceryItemDatabase
+{
+  public:
+    // Get a reference to the one and only instance of the database
+    static GroceryItemDatabase & instance();
+
+    // Locate and return a reference to a particular record
+    GroceryItem * find( const std::string & isbn );                             // Returns a pointer to the item in the database if
+                                                                                // found, nullptr otherwise
+    // Queries
+    std::size_t size() const;                                                   // Returns the number of items in the database
+
+  private:
+    GroceryItemDatabase            ( const std::string & filename );
+
+    GroceryItemDatabase            ( const GroceryItemDatabase & ) = delete;    // intentionally prohibit making copies
+    GroceryItemDatabase & operator=( const GroceryItemDatabase & ) = delete;    // intentionally prohibit copy assignments
+
+    ///////////////////////// TO-DO (2) //////////////////////////////
+      /// Private implementation details
+      /// Add any necessary private helper functions, member attributes, etc.
+    std::vector<GroceryItem> _groceryItem_database;
+    GroceryItem *            find( const std::string & upcCode, std::vector<GroceryItem>::iterator current_position);
+    /////////////////////// END-TO-DO (2) ////////////////////////////
+};
